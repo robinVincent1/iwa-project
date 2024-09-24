5357b60fc3f78ba165fcdb6cefc2ef93ea55dcc4
Lilian Monnereau <lilian.monnereau@gmail.com>
2024-09-24 23:59:26 +0200

    Maps with markers & initial region

--- app/views/map_view.tsx
+++ app/views/map_view.tsx
@@ -1,19 +1,64 @@
 import { StyleSheet,Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps'; 
+import MapView, { Marker } from 'react-native-maps'; 
+import React, { useRef } from 'react';
 
 export default function LocationMapView() {
+
+    const mapRef = useRef<any>(); //Changer le type plus tard
+
+    const INITIAL_REGION = {
+        latitude: 43.6,
+        longitude: 3.8833,
+        latitudeDelta: 0.3,
+        longitudeDelta: 0.3,
+    }
+
+    const markers = [
+        {
+            latitude: 43.6,
+            longitude: 3.8833,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+            name: "Montpellier"
+        },
+        {
+            latitude: 43.3,
+            longitude: 3.8833,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+            name: "Montpellier"
+        },
+        {
+            latitude: 43.5,
+            longitude: 3.8833,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+            name: "Montpellier"
+        },
+        {
+            latitude: 43.7,
+            longitude: 3.8833,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+            name: "Montpellier"
+        },
+    ]
+
     return (
         <View style={styles.container}>
             <MapView
             style={styles.map}
-            region={{
-                latitude: 43.6,
-                longitude: 3.8833,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-            }}
+            initialRegion={INITIAL_REGION}
+            ref={mapRef}
             >
+                {markers.map((marker, index) => (
+                    <Marker
+                        key={index}
+                        coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                        title={marker.name}
+                    />
+                ))}
             </MapView>
         </View>
     );
