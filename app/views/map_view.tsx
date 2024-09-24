c9b2937cf295849b30865bd30d310ed298eb3289
Lilian Monnereau <lilian.monnereau@gmail.com>
2024-09-24 23:45:55 +0200

    React native map setup

--- app/views/map_view.tsx
+++ app/views/map_view.tsx
@@ -1,20 +1,33 @@
 import { StyleSheet,Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import MapView from 'react-native-maps'; 
 
-export default function MapView() {
+export default function LocationMapView() {
     return (
         <View style={styles.container}>
-            <Text>Map</Text>
-            <StatusBar style="auto" />
+            <MapView
+            style={styles.map}
+            region={{
+                latitude: 43.6,
+                longitude: 3.8833,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+            >
+            </MapView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
     },
-});
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
